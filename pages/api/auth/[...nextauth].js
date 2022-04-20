b5ace937abe529768758a450596a94b3f2f5cf95
Ayush <84494951+ayussh04@users.noreply.github.com>
2022-04-21 01:01:09 +0530

    file

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,7 +4,7 @@ export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      secret: process.env.NEXT_PUBLIC_SECRET,
+      secret: process.env.NEXTAUTH_SECRET,
     }),
     // ...add more providers here
   ],
